654f086b2746354c8f86350b6c39b4aaa03d5868
Mike <mikeknueppel@hotmail.com>
2014-02-02 15:32:54 -0800

    Configure access keys for Amazon Web Services

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -15,6 +15,14 @@ Spree.config do |config|
   config.mail_auth_type = "Plain"
   config.smtp_username = ENV["GMAIL_USERNAME"]
   config.smtp_password = ENV["GMAIL_PASSWORD"]
+  config.use_s3 = true
+  config.s3_bucket = ENV['AWS_BUCKET_NAME']
+  config.s3_access_key = ENV["AWS_ACCESS_KEY_ID"]
+  config.s3_secret = ENV["AWS_SECRET_ACCESS_KEY"]
 end
 
 Spree.user_class = "Spree::LegacyUser"
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
