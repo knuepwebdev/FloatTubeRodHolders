c7da5a615e32e8939abb3d95eb1702972207f30f
Mike <mikeknueppel@hotmail.com>
2014-02-01 16:37:43 -0800

    Configure mailer. It works

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,9 +6,15 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.override_actionmailer_config = true
+  config.mails_from = "test@fishingusa.com"
+  config.mail_domain = "example.com"
+  config.mail_host = "smtp.gmail.com"
+  config.mail_port = 587
+  config.secure_connection_type = "TLS"
+  config.mail_auth_type = "Plain"
+  config.smtp_username = ENV["GMAIL_USERNAME"]
+  config.smtp_password = ENV["GMAIL_PASSWORD"]
 end
 
 Spree.user_class = "Spree::LegacyUser"
