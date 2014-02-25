2286dd5723045ac5e66919666a545a70437a5825
Mike <mikeknueppel@hotmail.com>
2014-02-25 12:45:26 -0800

    Order products within a taxonomy by name

---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,9 @@
+Spree::TaxonsController.class_eval do
+  def show
+	  @taxon = Spree::Taxon.find_by_permalink!(params[:id])
+	  return unless @taxon
+
+	  @searcher = build_searcher(params.merge(:taxon => @taxon.id))
+	  @products = @searcher.retrieve_products.order("name ASC")
+	end
+end
