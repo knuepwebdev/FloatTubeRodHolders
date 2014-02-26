3dd700fdd9f2d006ff11a2e0ed0bacd2eddf4e18
Mike <mikeknueppel@hotmail.com>
2014-02-25 18:46:08 -0800

    Make the logo a link to the home page

--- app/overrides/add_logo_to_header.rb
+++ app/overrides/add_logo_to_header.rb
@@ -1,6 +1,10 @@
 Deface::Override.new(
 	:virtual_path => 'spree/shared/_header',
 	:replace => 'figure#logo',
-	:text => "<figure id='logo' class='columns eight' data-hook><h1>FloatTubeRodHolders.com</h1></figure>",
+	:text => "<figure id='logo' class='columns eight' data-hook>
+							<h1>
+								<%= link_to Spree.t('FloatTubeRodHolders'), spree.root_path %>
+							</h1>
+						</figure>",
 	:name => 'add_logo_to_header'
 )
