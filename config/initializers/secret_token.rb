6a7c6ce5b1247dd878dd40f52c1cad9bdd0f656b
Mike <mikeknueppel@hotmail.com>
2014-02-25 20:20:00 -0800

    Set secret token in an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Spreeroundtwo::Application.config.secret_key_base = '86596cfded84834f8e26ddfbeeb423a4e8ca0ad9e10242a7189e6b50ea23e97eb932a1dfc6b32ccebc59d7b31e03891f7c26546cf1273154368016ef1156feda'
+Spreeroundtwo::Application.config.secret_key_base = ENV["RAILS_SECRET_TOKEN"]
